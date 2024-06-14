f5ea7757a099b2ad4ea1b1bad7b06e8cfc6f3c69
jggmwsntn <jggmwnstn@nexon.co.kr>
2024-06-14 18:01:38 +0900

    Draft Usage Sample

--- Source/Pointage/Painter.h
+++ Source/Pointage/Painter.h
@@ -6,30 +6,58 @@
 #include <vector>
 
 namespace Pointage {
-
     
-    class FArtSupply
+    
+    class FParameterPack
+    {
+
+    };
+
+    class FBlueprint 
     {
     public:
+        enum Type 
+        {
+            Figuration, // Compute
+            Draw        // Graphics
+        };
+
+    public:
+
+
 
     private:
-
-        std::vector<FDyestuffRef> Dystuffs;
-        std::vector<FBrushRef> Brushes;
-        std::vector<FCanvasRef> Canvases;
     };
 
     class FPainter
     {
     public:
 
+        void BeginWork() {}
 
-
-        template<typename Function>
-        void Dot(const Function& InFunction, const FArtSupply& InArtSupplies)
+        template<typename Function, typename FParameterPack>
+        void AddDotNode(const Function& InFunction, const FParameterPack& Params, FBlueprint* InBlueprint)
         {
-
+            InFunction(Param)
         }
 
+    private:
+        void Masterplanning() {}
+        void Craft() {}
+        void Exhibit() {}
     };
+
+    FPainter GSeurat;
+
+#define DEFINE_WORK_AS_FIGURATION()
+#define DEFINE_WORK_AS_DRAW()
+
+#define BEGIN_DEFINE_MATIERE_PACK(PackName)
+#define END_DEFINE_MATIERE_PACK
+
+#define MATIERE_OBJECT(Type, Name)
+#define MATIERE_BRUSH(Type, Name)
+#define MATIERE_DYESTUFF(Type, Name)
+#define MATIERE_CANVAS(Type, Name)
+
 }
--- Source/UsageSample.cpp
+++ Source/UsageSample.cpp
@@ -3,14 +3,44 @@
 
 #include "Pointage/Painter.h"
 
-void SampleRender(const Pointage::FPainter& PointagePainter)
-{
+namespace Pointage {
 
-    PointagePainter.
+    class FDeferredLihgtingBP
+        : public FBlueprint
+    {
+        DEFINE_WORK_AS_DRAW()
 
-    PointagePainter.AllocPoint([] {
-        FGraphTexture* RenderTargetTexture = AllocateTexture()
-        
-    });
+    BEGIN_DEFINE_MATIERE_PACK(FMatiere)
+        MATIERE_BRUSH(Texture2D, NormalMap) 
+        MATIERE_DYESTUFF(int32, Attenuation) 
+        MATIERE_CANVAS(Texture2D, GBufferA)
+    END_DEFINE_MATIERE_PACK
 
+    public:
+
+    };
+
+
+    void SampleDot()
+    {
+        FBlueprint* DefferedLightingBP = nullptr;
+
+        struct FParam 
+        {
+            int NormalMap;
+            int Attenuation;
+            int GBufferA;
+            bool bUseAttenuation;
+        };
+        FParam Param;
+
+        GSeurat.AddDotNode([](FDeferredLihgtingBP::FMatiere* Matiere, const FParam& InParam) {
+
+            Matiere->NormalMap = InParam.NormalMap;
+            Matiere->Attenuation = InParam.Attenuation;
+            Matiere->GBufferA = InParam.GBufferA;
+
+        }, Param, DefferedLightingBP);
+
+    }
 }
